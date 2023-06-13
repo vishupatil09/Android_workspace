a674f9a38c8fe4110115311de41cd28a351cb396
Vishal Patil <111958932+vishupatil09@users.noreply.github.com>
2023-06-13 11:50:59 +0530

    Update ascii1.kt

--- Module/task2/ascii1.kt
+++ Module/task2/ascii1.kt
@@ -1,8 +1,14 @@
 fun main()
 {
+    println("Enter Float value: ")
+    var  d = readln().toFloat()
+
+
   val c = 'a'
    val ascii = c.toInt()
    println(" the Value of $c is : $ascii")
+val e = d * ascii
+    println("your float number is : $e")
 
 
 
