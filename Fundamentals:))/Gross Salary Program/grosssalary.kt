90750b1b9c8f0859f9b1ca0a955b0f6a43d60f68
Vishal Patil <111958932+vishupatil09@users.noreply.github.com>
2023-11-01 13:30:59 +0530

    Add files via upload

---
+++ Fundamentals:))/Gross Salary Program/grosssalary.kt
@@ -0,0 +1,16 @@
+fun main(){
+    println("Enter The Your Salary: ")
+    var salary = readln().toInt()
+
+
+    var primium = salary/10
+
+    var loan = salary/10
+
+
+var gross = salary-(primium+loan)
+
+println("Your Gross Salary is : $gross")
+
+
+}
