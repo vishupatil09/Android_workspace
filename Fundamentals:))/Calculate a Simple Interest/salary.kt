42e8f8f04707024c091a0cd9c1e6d7678b35777d
Vishal Patil <111958932+vishupatil09@users.noreply.github.com>
2023-11-01 12:47:23 +0530

    Add files via upload

---
+++ Fundamentals:))/Calculate a Simple Interest/salary.kt
@@ -0,0 +1,13 @@
+fun main(){
+
+    println("Enter Your  monthly Sallery: ")
+
+    var salary= readln().toFloat()
+
+    var anual = salary*12
+
+    println("Your Annual salary is : $anual ")
+
+
+}
+
