ff6f093a0fd7554ef8eb8db0f7d0192b8e5df3a3
Vishal Patil <111958932+vishupatil09@users.noreply.github.com>
2023-11-02 14:01:30 +0530

    Add files via upload

---
+++ Fundamentals:))/power/Powers.kt
@@ -0,0 +1,12 @@
+fun main()
+{
+    println("Enter The number younwant power: ")
+
+    var a = readln().toInt()
+
+    var b = a*a
+    var c = a*a*a
+
+
+    println("Entered number power 1 is $a , power 2 is $b and  power by 3 is $c")
+}
